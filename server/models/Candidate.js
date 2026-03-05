dc49e2b674f672fdf1a9465356207f23e6b1711c
Gajula <supreethi.gajula@accenture.com>
2026-03-05 15:48:14 +0530

    Candidate schema skeleton added

--- server/models/Candidate.js
+++ server/models/Candidate.js
@@ -2,5 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const candidateSchema = new Schema({
-    
-})
+    name:{type : String,required : true},
+    email: { type: String, required: true, unique: true },
+    experience: { type: Number, required: true },
+    skills: { type: [String], required: true },
+    targetRole: { type: String, required: true },
+    dsaScore: { type: Number, default: 0 },
+    systemDesignScore: { type: Number, default: 0 },
+    projectScore: { type: Number, default: 0 },
+    hrScore: { type: Number, default: 0 },
+    finalScore: { type: Number, default: 0 },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Candidate',candidateSchema);
